103a64dbcfe747e44776fadf0fc7919beb0581ac
bowook <mbosx316@naver.com>
2024-12-02 20:10:07 +0900

    feat(RecommendService) : 추천 서비스 기능 추가

--- src/main/java/menu/service/RecommendService.java
+++ src/main/java/menu/service/RecommendService.java
@@ -1,2 +1,49 @@
-package menu.service;public class RecommendService {
+package menu.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import menu.domain.Coach;
+import menu.domain.Coaches;
+import menu.domain.constants.Category;
+import menu.domain.constants.Menu;
+
+public class RecommendService {
+
+    public List<Category> recommend(Coaches coaches) {
+        List<Category> categories = recommendCategory();
+        recommendMenuForEachCoach(coaches.getCoaches(), categories);
+        return categories;
+    }
+
+    private void recommendMenuForEachCoach(List<Coach> coaches, List<Category> categories) {
+        for (Coach coach : coaches) {
+            recommendMenu(categories, coach);
+        }
+    }
+
+    private void recommendMenu(List<Category> categories, Coach coach) {
+        for (Category category : categories) {
+            List<String> tempMenus = Menu.findByCategory(category);
+            Menu menu = Menu.from(Randoms.shuffle(tempMenus).get(0));
+            while (!coach.addEatMenu(menu)) {
+                menu = Menu.from(Randoms.shuffle(tempMenus).get(0));
+            }
+        }
+    }
+
+    private List<Category> recommendCategory() {
+        List<Category> categories = new ArrayList<>();
+        Map<Category, Integer> categoryMap = new HashMap<>();
+        while (categories.size() < 5) {
+            Category category = Category.from(Randoms.pickNumberInRange(1, 5));
+            categoryMap.put(category, categoryMap.getOrDefault(category, 0) + 1);
+            if (categoryMap.get(category) <= 2) {
+                categories.add(category);
+            }
+        }
+        return categories;
+    }
 }
