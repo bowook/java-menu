5fe45ba040fafc41a6ad32db200b900d409adc25
bowook <mbosx316@naver.com>
2024-12-02 20:08:02 +0900

    feat(Coaches) : 코치 리스트 도메인 추가

--- src/main/java/menu/domain/Coaches.java
+++ src/main/java/menu/domain/Coaches.java
@@ -1,2 +1,17 @@
-package menu.domain;public class Coaches {
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Coaches {
+    private final List<Coach> coaches = new ArrayList<>();
+
+    public void addCoach(Coach coach) {
+        coaches.add(coach);
+    }
+
+    public List<Coach> getCoaches() {
+        return Collections.unmodifiableList(coaches);
+    }
 }
