875380f072a018c6a7bcc72de3f6a8a6e12f3ec7
bowook <mbosx316@naver.com>
2024-12-02 20:08:50 +0900

    feat(Day) : 요일 도메인 추가

--- src/main/java/menu/domain/constants/Day.java
+++ src/main/java/menu/domain/constants/Day.java
@@ -1,2 +1,30 @@
-package menu.domain.constants;public class Day {
+package menu.domain.constants;
+
+public enum Day {
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일");
+
+    private final String name;
+
+    Day(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static String result() {
+        StringBuilder dayBuilder = new StringBuilder();
+        dayBuilder.append("[ 구분 | ");
+        for (Day day : Day.values()) {
+            dayBuilder.append(day.getName()).append(" | ");
+        }
+        dayBuilder.setLength(dayBuilder.length() - 3);
+        dayBuilder.append(" ]");
+        return dayBuilder.toString();
+    }
 }
