1d6e8fbc084cc227c4f7e6f06e8442074cdb945a
bowook <mbosx316@naver.com>
2024-12-02 20:07:16 +0900

    feat(MenuController) : 컨트롤러 기능 추가

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,2 +1,50 @@
-package menu.controller;public class MenuController {
+package menu.controller;
+
+import java.util.List;
+import menu.domain.Coaches;
+import menu.domain.constants.Category;
+import menu.exception.CustomException;
+import menu.service.RecommendService;
+import menu.view.InputView;
+import menu.view.OutputView;
+
+public class MenuController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RecommendService recommendService;
+
+    public MenuController(final InputView inputView, final OutputView outputView, final RecommendService recommend) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.recommendService = recommend;
+    }
+
+    public void start() {
+        outputView.printStartMenuRecommend();
+        Coaches coaches = readCoaches();
+        readCantEat(coaches);
+        List<Category> categories = recommendService.recommend(coaches);
+        outputView.printResult(coaches, categories);
+    }
+
+    private void readCantEat(Coaches coaches) {
+        while (true) {
+            try {
+                inputView.readCantEat(coaches);
+                break;
+            } catch (CustomException customException) {
+                outputView.printErrorMessage(customException.getMessage());
+            }
+        }
+    }
+
+    private Coaches readCoaches() {
+        while (true) {
+            try {
+                return inputView.readCoachNames();
+            } catch (CustomException customException) {
+                outputView.printErrorMessage(customException.getMessage());
+            }
+        }
+    }
 }
